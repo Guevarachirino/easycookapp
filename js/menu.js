5d5004c6669329e1e2ba3f0a60b0ea5ae5f61b4a
Guevarachirino <chi23017@byui.edu>
2026-02-09 12:09:26 -0500

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,15 @@
-const hamburger = document.getElementById("hamburger");
-const menu = document.getElementById("menu");
-const links = menu.querySelectorAll("a");
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const menu = document.getElementById("menu");
+  const links = menu.querySelectorAll("a");
 
-hamburger.addEventListener("click", () => {
-  menu.classList.toggle("active");
-});
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
 
-links.forEach(link => {
-  link.addEventListener("click", () => {
-    menu.classList.remove("active");
+  links.forEach(link => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("active");
+    });
   });
 });
